b29dcc728291cff05a112b73b4dcc47622d82542
TERRYYYY <kariuki.terry11@gmail.com>
2024-05-17 15:49:20 +0300

    Calling the getAllPosts API

--- src/app/service/post.service.ts
+++ src/app/service/post.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-const BASIC_URL = "http://localhost:8000/";
+const BASIC_URL = "http://localhost:8080/";
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +11,13 @@ export class PostService {
 
   constructor(private http: HttpClient) { }
 
-// Method that handles HTTP requests to get all posts
+// Method that handles HTTP requests to create new posts
   createNewPostService(data:any): Observable<any>{     //Observable -> A data stream you can subscribe to handle event handling & asynchronous responses
     return this.http.post(BASIC_URL + 'api/posts', data);
+  }
+
+  // Method that handles HTTP requests to get all posts
+  getallPosts():Observable<any>{
+    return this.http.get(BASIC_URL + 'api/posts');
   }
 }
