2991c7ef802bbcafef898076015616d61c693ff7
TERRYYYY <kariuki.terry11@gmail.com>
2024-05-20 14:32:18 +0300

    Created a form to post new comments on each posts

--- src/app/pages/view-post/view-post.component.ts
+++ src/app/pages/view-post/view-post.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { PostService } from '../../service/post.service';
 import { ActivatedRoute } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommentService } from '../../service/comment.service';
 
 @Component({
   selector: 'app-view-post',
@@ -14,15 +16,25 @@ export class ViewPostComponent {
   postId = this.activatedRoute.snapshot.params['id'];
   postData: any;
 
+  //Comment Form Group
+  commentForm! : FormGroup
+
   constructor(
     private postService: PostService,
     private activatedRoute:ActivatedRoute,
-    private matSnackBar:MatSnackBar
+    private matSnackBar:MatSnackBar, 
+    private myForm: FormBuilder,
+    private commentService: CommentService,
   ){}
 
   ngOnInit(){
     console.log(this.postId);
     this.getPostById();
+
+    this.commentForm = this.myForm.group({
+      postedBy: [null, Validators.required],
+      content: [null, Validators.required]
+    })
   }
 
   //Method to get post by ID
@@ -41,6 +53,19 @@ export class ViewPostComponent {
       this.matSnackBar.open("Post Liked", "Ok");
       this.getPostById();
     }, (error) => {
+      this.matSnackBar.open("Something went wrong", "Ok");
+    })
+  }
+
+  //Method to publish a new comment
+  publishComment(){
+    const postedBy = this.commentForm.get('postedBy')?.value;
+    const content = this.commentForm.get('content')?.value;
+
+    this.commentService.createNewComment(this.postId, postedBy, content).subscribe(res=>{
+      this.matSnackBar.open("Comment Published", "Ok");
+      // this.commentForm.reset();
+    }, error=>{
       this.matSnackBar.open("Something went wrong", "Ok");
     })
   }
